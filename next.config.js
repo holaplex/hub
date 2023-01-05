fa6c821fe7fad60d65d208c69f306107ff28645a
Anshul Goel <anshul.go87@gmail.com>
2023-01-05 21:02:47 +0530

    setup

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  i18n: {
+    defaultLocale: 'en-US',
+    locales: ['en-US'],
+  },
+  reactStrictMode: true,
+};
+
+module.exports = nextConfig;
