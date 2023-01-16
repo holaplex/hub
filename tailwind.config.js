c0cacdafc43a279c8a4205490ce088b68cd539d7
Anshul Goel <anshul.go87@gmail.com>
2023-01-16 19:43:34 +0530

    add color theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 module.exports = {
   content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        positive: '#479E5A',
+        negative: '#E62E2E',
+        warning: '#F0B41A',
+        gray: {
+          50: '#f5f5f5',
+          100: '#e6e6e6',
+          200: 'с5с5с5',
+          500: '#7b7b7b',
+          600: '#555555',
+          800: '#292929',
+        },
+      },
+    },
   },
   plugins: [],
 };
