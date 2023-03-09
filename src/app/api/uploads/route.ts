feef11616a499c58ed959aabb2759bad4fc0f8ff
Anshul Goel <anshul.go87@gmail.com>
2023-03-09 21:31:01 +0530

    file upload api fix, update formidable, still need to fix formidable error

--- src/app/api/uploads/route.ts
+++ src/app/api/uploads/route.ts
@@ -1,5 +1,5 @@
 import { NextApiRequest } from 'next';
-import formidable, { Fields, Files } from 'formidable';
+import { Fields, Files, IncomingForm } from 'formidable';
 import { NextResponse } from 'next/server';
 import { ok } from 'assert';
 import { is, map, pipe, take, values, when } from 'ramda';
@@ -20,17 +20,20 @@ async function uploadFile(file: File): Promise<string> {
 }
 
 export async function POST(req: NextApiRequest) {
+  console.log('file uploads request', req);
   const formResult = await new Promise<FormData>((resolve, reject) => {
-    const form = new formidable.IncomingForm({ keepExtensions: true });
+    const form = new IncomingForm({ keepExtensions: true });
     form.parse(req, async (err, fields, files) => {
       console.log('form err', err);
       if (err) reject(err);
       ok({ fields, files });
     });
   });
+  console.log('file uploads formresult', formResult);
   const payload = pipe(values, map(when(is(Array), take(0))))(formResult.files) as File[];
+  console.log('file uploads payload', payload);
   const uploadPromises = payload.map((file) => uploadFile(file as File));
   const files = await Promise.all(uploadPromises);
-
+  console.log('file uploads files', files);
   return NextResponse.json(files);
 }
