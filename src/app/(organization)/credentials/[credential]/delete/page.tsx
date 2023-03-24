8c00c3f9af2aa672b95f6e091569588b3e21e7e8
Anshul Goel <anshul.go87@gmail.com>
2023-03-24 20:16:47 +0530

    delete credential ui update, add loading states to delete webhook (#85)

--- src/app/(organization)/credentials/[credential]/delete/page.tsx
+++ src/app/(organization)/credentials/[credential]/delete/page.tsx
@@ -1,26 +1,119 @@
 'use client';
-import { Button, Form, Modal } from '@holaplex/ui-library-react';
+import { useMutation, useQuery } from '@apollo/client';
+import { Button, Modal } from '@holaplex/ui-library-react';
+import { useRouter } from 'next/navigation';
 import Card from '../../../../../components/Card';
 import Typography, { Size } from '../../../../../components/Typography';
+import {
+  DeleteCredentialInput,
+  DeleteCredentialPayload,
+  Organization,
+} from '../../../../../graphql.types';
+import { useOrganization } from '../../../../../hooks/useOrganization';
+import {
+  GetOrganizationCredential,
+  GetOrganizationCredentials,
+} from './../../../../../queries/credentials.graphql';
+import { DeleteCredential } from './../../../../../mutations/credential.graphql';
 
-export default function DeleteCredential() {
+interface DeleteCredentialData {
+  deleteCredential: DeleteCredentialPayload;
+}
+
+interface DeleteCredentialVars {
+  input: DeleteCredentialInput;
+}
+interface GetCredentialData {
+  organization: Organization;
+}
+
+interface GetCredentialVars {
+  credential: string;
+  organization: string;
+}
+interface DeleteCredentialProps {
+  params: { credential: string };
+}
+
+export default function DeleteCredentialPage({ params: { credential } }: DeleteCredentialProps) {
+  const { organization } = useOrganization();
+  const router = useRouter();
+
+  const onClose = () => {
+    router.push('/credentials');
+  };
+
+  const credentialQuery = useQuery<GetCredentialData, GetCredentialVars>(
+    GetOrganizationCredential,
+    {
+      variables: { credential, organization: organization?.id },
+    }
+  );
+  const [deleteCredential, { loading }] = useMutation<DeleteCredentialData, DeleteCredentialVars>(
+    DeleteCredential,
+    {
+      refetchQueries: [
+        { query: GetOrganizationCredentials, variables: { organization: organization?.id } },
+      ],
+    }
+  );
+
+  const onDelete = () => {
+    deleteCredential({
+      variables: {
+        input: {
+          credential,
+        },
+      },
+      onCompleted: () => {
+        router.push('/credentials');
+      },
+    });
+  };
   return (
-    <Modal open={false} setOpen={(open: boolean) => {}}>
+    <Modal open={true} setOpen={onClose}>
       <Card className="w-[400px]">
         <Typography.Header size={Size.H2}>Delete access token</Typography.Header>
-        <Typography.Header size={Size.H3}>
-          Are you sure you want to delete [name] access token and all its contents? You will not be
-          able to recover this access token or its contents. This operation can not be undone.
-        </Typography.Header>
+        {credentialQuery.loading ? (
+          <>
+            <div className="mt-2 flex flex-col gap-1">
+              <div className="w-full h-4 rounded-md bg-gray-100 animate-pulse" />
+              <div className="w-full h-4 rounded-md bg-gray-100 animate-pulse" />
+              <div className="w-full h-4 rounded-md bg-gray-100 animate-pulse" />
+              <div className="w-full h-4 rounded-md bg-gray-100 animate-pulse" />
+            </div>
+            <div className="mt-4 flex flex-col gap-2">
+              <div className="w-full h-10 rounded-md bg-gray-100 animate-pulse" />
+              <div className="w-full h-10 rounded-md bg-gray-100 animate-pulse" />
+            </div>
+          </>
+        ) : (
+          <>
+            <Typography.Header size={Size.H3} className="mt-2">
+              Are you sure you want to delete{' '}
+              <span className="text-primary font-medium">
+                {credentialQuery.data?.organization.credential?.name}
+              </span>{' '}
+              access token and all its contents? You will not be able to recover this access token
+              or its contents. This operation can not be undone.
+            </Typography.Header>
 
-        <Form className="flex flex-col mt-5">
-          <Button htmlType="submit" className="w-full mt-5" variant="failure">
-            Delete
-          </Button>
-          <Button variant="tertiary" className="w-full mt-5 " onClick={() => {}}>
-            Cancel
-          </Button>
-        </Form>
+            <div className="flex flex-col gap-2 mt-4">
+              <Button
+                htmlType="submit"
+                className="w-full mt-5"
+                variant="failure"
+                onClick={onDelete}
+                disabled={loading}
+              >
+                Delete
+              </Button>
+              <Button variant="tertiary" className="w-full" onClick={onClose} disabled={loading}>
+                Cancel
+              </Button>
+            </div>
+          </>
+        )}
       </Card>
     </Modal>
   );
