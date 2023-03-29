ead93f8466811ecb589d46c635c0f0c8e5e87d40
Anshul Goel <anshul.go87@gmail.com>
2023-03-29 16:13:38 +0530

    edit drop page

---
+++ src/app/(project)/projects/[project]/drops/[drop]/edit/page.tsx
@@ -0,0 +1,77 @@
+'use client';
+import { useMutation, useQuery } from '@apollo/client';
+import { Button, Modal } from '@holaplex/ui-library-react';
+import { useRouter } from 'next/navigation';
+import Card from '../../../../../../../components/Card';
+import Typography, { Size } from '../../../../../../../components/Typography';
+import { GetDropBasicDetail } from './../../../../../../../queries/drop.graphql';
+import { PauseDrop } from './../../../../../../../mutations/drop.graphql';
+import { Project, PauseDropInput, PauseDropPayload } from '../../../../../../../graphql.types';
+
+interface GetDropData {
+  project: Project;
+}
+
+interface GetDropVars {
+  drop: string;
+  project: string;
+}
+interface EditDropProps {
+  params: { drop: string; project: string };
+}
+
+export default function EditDropPage({ params: { drop, project } }: EditDropProps) {
+  const router = useRouter();
+
+  const onClose = () => {
+    router.push(`/projects/${project}/drops/`);
+  };
+
+  const dropQuery = useQuery<GetDropData, GetDropVars>(GetDropBasicDetail, {
+    variables: { drop, project },
+  });
+
+  const onEdit = () => {};
+
+  return (
+    <Modal open={true} setOpen={onClose}>
+      <Card className="w-[400px]">
+        <Typography.Header size={Size.H2}>Edit drop</Typography.Header>
+        {dropQuery.loading ? (
+          <>
+            <div className="mt-2 flex flex-col gap-1">
+              <div className="w-full h-4 rounded-md bg-gray-100 animate-pulse" />
+            </div>
+            <div className="mt-4 flex flex-col gap-2">
+              <div className="w-full h-10 rounded-md bg-gray-100 animate-pulse" />
+              <div className="w-full h-10 rounded-md bg-gray-100 animate-pulse" />
+              <div className="w-full h-10 rounded-md bg-gray-100 animate-pulse" />
+              <div className="w-full h-10 rounded-md bg-gray-100 animate-pulse" />
+            </div>
+          </>
+        ) : (
+          <>
+            <Typography.Header size={Size.H3} className="mt-2">
+              Select what you want to edit in this drop below
+            </Typography.Header>
+
+            <div className="flex flex-col gap-2 mt-4">
+              <Button
+                htmlType="submit"
+                className="w-full mt-5"
+                variant="failure"
+                onClick={onEdit}
+                disabled={loading}
+              >
+                Pause mint
+              </Button>
+              <Button variant="tertiary" className="w-full" onClick={onClose} disabled={loading}>
+                Cancel
+              </Button>
+            </div>
+          </>
+        )}
+      </Card>
+    </Modal>
+  );
+}
