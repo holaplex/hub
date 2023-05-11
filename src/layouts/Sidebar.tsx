a55d4cd8c820d9f406a9b9af286506b0be0b004a
Anshul Goel <anshul.go87@gmail.com>
2023-05-11 15:15:43 +0530

    Hub logo, beta symbol, sidebar menu link issue (#176)
    * hub logo, beta symbol, sidebar menu link issue
    
    * refactor: add shadow to user menu box
    
    ---------
    
    Co-authored-by: Kyle Espinola <kyle.s.espinola@gmail.com>

--- src/layouts/Sidebar.tsx
+++ src/layouts/Sidebar.tsx
@@ -82,16 +82,20 @@ interface SidebarMenuItemProps {
 
 function SidebarMenuLink({ icon, name, href, active, className, target }: SidebarMenuItemProps) {
   return (
-    <Link
-      href={href}
-      className={clsx('flex gap-6 w-full px-4 py-3 items-center rounded-lg text-white', className, {
-        'bg-stone-800': active,
-        'hover:bg-stone-800': !active,
-      })}
-      target={target}
-    >
-      {icon}
-      <span className="text-sm">{name}</span>
+    <Link href={href} target={target}>
+      <div
+        className={clsx(
+          'flex gap-6 w-full px-4 py-3 items-center rounded-lg text-white',
+          className,
+          {
+            'bg-stone-800': active,
+            'hover:bg-stone-800': !active,
+          }
+        )}
+      >
+        {icon}
+        <span className="text-sm">{name}</span>
+      </div>
     </Link>
   );
 }
@@ -149,126 +153,130 @@ function SidebarFooter({ organization, children, className }: SidebarFooterProps
   });
 
   return (
-    <div className={clsx('flex flex-col w-full p-6 flex-shrink-0 gap-2', className)}>
-      <div
-        className="flex items-center gap-2 justify-between cursor-pointer pt-2"
-        onClick={() => {
-          if (!userAffiliationsQuery.called) {
-            loadUserAffiliations();
-          }
-
-          setExpandFooter(!expandFooter);
-        }}
-      >
-        <h1 className="flex items-center gap-2 text-sm text-white font-medium">
-          <div className="w-8 h-8 bg-gray-800 rounded-full" />
-          <span className="flex flex-col capitalize">
-            {userQuery.loading ? (
-              <div className="h-4 w-20 bg-stone-800 rounded-full animate-pulse" />
-            ) : (
-              `${userQuery.data?.user.firstName} ${userQuery.data?.user.lastName}`
-            )}
-            <span className="text-gray-400 text-xs">{organization?.name}</span>
-          </span>
-        </h1>
-        {expandFooter ? <Icon.Dropdown /> : <Icon.DropdownReverse />}
+    <div className={clsx('flex flex-col w-full flex-shrink-0 gap-2 pt-4', className)}>
+      <div className="flex justify-center gap-2 items-center mb-4">
+        <Icon.HubLogo />
+        <Icon.Beta />
       </div>
-      {expandFooter && (
-        <div className="w-full py-2">
-          {userAffiliationsQuery.loading ? (
-            <div className="flex flex-col gap-6">
-              <div className="flex gap-2 items-center">
-                <div className="w-8 h-8 bg-stone-800 rounded-md animate-pulse" />
-                <span className="rounded-full h-4 w-28 bg-stone-800 animate-pulse" />
+      <div className="shadow-[0px_0px_16px_rgba(0,0,0,0.35)] rounded-t-lg p-6">
+        <div
+          className="flex items-center gap-2 justify-between cursor-pointer pt-2"
+          onClick={() => {
+            if (!userAffiliationsQuery.called) {
+              loadUserAffiliations();
+            }
+
+            setExpandFooter(!expandFooter);
+          }}
+        >
+          <h1 className="flex items-center gap-2 text-sm text-white font-medium">
+            <div className="w-8 h-8 bg-gray-800 rounded-full" />
+            <span className="flex flex-col capitalize">
+              {userQuery.loading ? (
+                <div className="h-4 w-20 bg-stone-800 rounded-full animate-pulse" />
+              ) : (
+                `${userQuery.data?.user.firstName} ${userQuery.data?.user.lastName}`
+              )}
+              <span className="text-gray-400 text-xs">{organization?.name}</span>
+            </span>
+          </h1>
+          {expandFooter ? <Icon.Dropdown /> : <Icon.DropdownReverse />}
+        </div>
+        {expandFooter && (
+          <div className="w-full py-2">
+            {userAffiliationsQuery.loading ? (
+              <div className="flex flex-col gap-6">
+                <div className="flex gap-2 items-center">
+                  <div className="w-8 h-8 bg-stone-800 rounded-md animate-pulse" />
+                  <span className="rounded-full h-4 w-28 bg-stone-800 animate-pulse" />
+                </div>
+                <div className="flex gap-2 items-center">
+                  <div className="w-8 h-8 bg-stone-800 rounded-md animate-pulse" />
+                  <span className="rounded-full h-4 w-28 bg-stone-800 animate-pulse" />
+                </div>
               </div>
-              <div className="flex gap-2 items-center">
-                <div className="w-8 h-8 bg-stone-800 rounded-md animate-pulse" />
-                <span className="rounded-full h-4 w-28 bg-stone-800 animate-pulse" />
-              </div>
-            </div>
-          ) : (
-            <div className="flex flex-col gap-6">
-              <div className="flex flex-col border-b border-gray-800 py-2">
-                {/* <Sidebar.Menu.Link
+            ) : (
+              <div className="flex flex-col gap-6">
+                <div className="flex flex-col border-b border-gray-800 py-2">
+                  {/* <Sidebar.Menu.Link
                   name="Settings"
                   icon={<Icon.Settings />}
                   href=""
                   active={false}
                 /> */}
-                <Sidebar.Menu.Link
-                  name="Help"
-                  icon={<Icon.HelpHeadphones stroke="stroke-gray-400" />}
-                  href="https://docs.holaplex.dev"
-                  active={false}
-                  target="_blank"
-                />
+                  <Sidebar.Menu.Link
+                    name="Help"
+                    icon={<Icon.HelpHeadphones stroke="stroke-gray-400" />}
+                    href="https://docs.holaplex.dev"
+                    active={false}
+                    target="_blank"
+                  />
 
-                <div
-                  className="flex gap-6 w-full px-4 py-3 items-center text-gray-400 rounded-lg hover:bg-stone-800 cursor-pointer"
-                  onClick={() => logout()}
-                >
-                  <Icon.Logout stroke="stroke-gray-400" />
-                  <span className="text-sm text-white">Logout</span>
+                  <div
+                    className="flex gap-6 w-full px-4 py-3 items-center text-gray-400 rounded-lg hover:bg-stone-800 cursor-pointer"
+                    onClick={() => logout()}
+                  >
+                    <Icon.Logout stroke="stroke-gray-400" />
+                    <span className="text-sm text-white">Logout</span>
+                  </div>
                 </div>
-              </div>
-              <div className="flex flex-col gap-2 max-h-60 overflow-y-auto">
-                {userAffiliationsQuery.data?.user.affiliations.map((affiliation) => {
-                  return (
-                    <div
-                      key={affiliation.organization?.id}
-                      className={clsx(
-                        'flex items-center rounded-lg justify-between p-2 transition',
-                        {
-                          'bg-stone-800': affiliation.organization?.id === organization?.id,
-                          'cursor-pointer hover:bg-stone-800':
-                            affiliation.organization?.id !== organization?.id,
-                        }
-                      )}
-                    >
+                <div className="flex flex-col gap-2 max-h-60 overflow-y-auto">
+                  {userAffiliationsQuery.data?.user.affiliations.map((affiliation) => {
+                    return (
                       <div
-                        className="flex gap-2 items-center"
+                        key={affiliation.organization?.id}
                         onClick={() => onSwitch(affiliation.organization?.id)}
-                      >
-                        {affiliation.organization?.profileImageUrl ? (
-                          <img
-                            className="w-8 h-8 rounded-md"
-                            src={affiliation.organization.profileImageUrl}
-                            alt="logo"
-                          />
-                        ) : (
-                          <div className="w-8 h-8 bg-stone-800 rounded-md" />
+                        className={clsx(
+                          'flex items-center rounded-lg justify-between p-2 transition',
+                          {
+                            'bg-stone-800': affiliation.organization?.id === organization?.id,
+                            'cursor-pointer hover:bg-stone-800':
+                              affiliation.organization?.id !== organization?.id,
+                          }
                         )}
-                        <span className="text-gray-400 font-medium text-sm">
-                          {affiliation.organization?.name}
-                        </span>
+                      >
+                        <div className="flex gap-2 items-center">
+                          {affiliation.organization?.profileImageUrl ? (
+                            <img
+                              className="w-8 h-8 rounded-md"
+                              src={affiliation.organization.profileImageUrl}
+                              alt="logo"
+                            />
+                          ) : (
+                            <div className="w-8 h-8 bg-stone-800 rounded-md" />
+                          )}
+                          <span className="text-gray-400 font-medium text-sm">
+                            {affiliation.organization?.name}
+                          </span>
+                        </div>
                       </div>
-                    </div>
-                  );
-                })}
+                    );
+                  })}
+                </div>
+                <Link href="/organizations/new">
+                  <Button
+                    icon={<Icon.Add stroke="stroke-yellow-300" />}
+                    variant="secondary"
+                    className="w-full"
+                  >
+                    Add organization
+                  </Button>
+                </Link>
+                <span className="flex gap-1 w-full text-gray-400 text-xs justify-center items-center">
+                  <Link href="/terms-of-service" target="_blank">
+                    Terms of service
+                  </Link>
+                  -
+                  <Link href="/privacy-policy" target="_blank">
+                    Privacy Policy
+                  </Link>
+                </span>
               </div>
-              <Link href="/organizations/new">
-                <Button
-                  icon={<Icon.Add stroke="stroke-yellow-300" />}
-                  variant="secondary"
-                  className="w-full"
-                >
-                  Add organization
-                </Button>
-              </Link>
-              <span className="flex gap-1 w-full text-gray-400 text-xs justify-center items-center">
-                <Link href="/terms-of-service" target="_blank">
-                  Terms of service
-                </Link>
-                -
-                <Link href="/privacy-policy" target="_blank">
-                  Privacy Policy
-                </Link>
-              </span>
-            </div>
-          )}
-        </div>
-      )}
-      {children}
+            )}
+          </div>
+        )}
+        {children}
+      </div>
     </div>
   );
 }
