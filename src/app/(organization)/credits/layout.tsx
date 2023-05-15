c0f33d32fafefbc7c79312fe6550b41b939cb5c2
Kyle Espinola <kyle.s.espinola@gmail.com>
2023-05-15 22:04:00 +0200

    fix: query credit sheet in separate query so skip and authorized fields are not in the same query

--- src/app/(organization)/credits/layout.tsx
+++ src/app/(organization)/credits/layout.tsx
@@ -4,7 +4,10 @@ import { usePathname } from 'next/navigation';
 import { cloneElement, useMemo } from 'react';
 import { Icon } from '../../../components/Icon';
 import Tabs from '../../../layouts/Tabs';
-import { GetOrganizationCreditAndDeductionTotals } from './../../../queries/credits.graphql';
+import {
+  GetOrganizationCreditAndDeductionTotals,
+  GetCreditSheet,
+} from './../../../queries/credits.graphql';
 import { GetOrganizationDrops } from './../../../queries/drop.graphql';
 import { useQuery } from '@apollo/client';
 import { useOrganization } from '../../../hooks/useOrganization';
@@ -27,6 +30,9 @@ interface GetOrganizationDropsVars {
 
 interface GetOrganizationDropsData {
   organization: Organization;
+}
+
+interface GetCreditSheetData {
   creditSheet: ActionCost[];
 }
 
@@ -56,8 +62,10 @@ export default function CreditsLayout({
     }
   );
 
+  const creditSheetQuery = useQuery<GetCreditSheetData>(GetCreditSheet);
+
   const projects = dropsQuery.data?.organization.projects;
-  const creditSheet = dropsQuery.data?.creditSheet;
+  const creditSheet = creditSheetQuery.data?.creditSheet;
 
   const estimatedCost = useMemo(() => {
     const costLookup = new CreditLookup(creditSheet || []);
@@ -76,7 +84,8 @@ export default function CreditsLayout({
     );
   }, [creditSheet, projects]);
 
-  const loading = creditAndDeductionsQuery.loading || dropsQuery.loading;
+  const loading =
+    creditAndDeductionsQuery.loading || dropsQuery.loading || creditSheetQuery.loading;
 
   return (
     <div className="h-full flex flex-col p-4">
