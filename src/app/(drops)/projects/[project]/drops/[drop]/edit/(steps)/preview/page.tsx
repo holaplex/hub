35c2056822da6ed29bef8be2e6ea15965deb006e
Anshul Goel <anshul.go87@gmail.com>
2023-05-16 20:09:04 +0530

    Animation URL and Drop Preview Redesign (#184)
    * create drop preview page update
    
    * add include video url and new preview ui to edit drop
    
    * touchups and fixes
    
    * error handling and more ui fixes
    
    * refactor: show video preview. user cant adjust the supply so don't make it an input field.
    
    ---------
    
    Co-authored-by: Kyle Espinola <kyle.s.espinola@gmail.com>

--- src/app/(drops)/projects/[project]/drops/[drop]/edit/(steps)/preview/page.tsx
+++ src/app/(drops)/projects/[project]/drops/[drop]/edit/(steps)/preview/page.tsx
@@ -1,266 +1,225 @@
 'use client';
-import { Button } from '@holaplex/ui-library-react';
+import { Button, Form, Placement } from '@holaplex/ui-library-react';
 import { useRouter } from 'next/navigation';
-import { toast } from 'react-toastify';
+import Dropzone from 'react-dropzone';
+import { Controller, useForm, useFieldArray } from 'react-hook-form';
 import Card from '../../../../../../../../../components/Card';
+import { Icon } from '../../../../../../../../../components/Icon';
 import Typography, { Size } from '../../../../../../../../../components/Typography';
-import {
-  ActionCost,
-  CreatorInput,
-  Organization,
-  PatchDropInput,
-  PatchDropPayload,
-} from '../../../../../../../../../graphql.types';
-import { StoreApi, useStore } from 'zustand';
-import { ApolloError, useMutation, useQuery } from '@apollo/client';
-import { format } from 'date-fns';
-import { File } from 'nft.storage';
-import { PatchDrop } from './../../../../../../../../../mutations/drop.graphql';
-import { combineDateTime, maybeToUtc, DateFormat } from '../../../../../../../../../modules/time';
+import { Blockchain } from '../../../../../../../../../graphql.types';
 import { useProject } from '../../../../../../../../../hooks/useProject';
-import { useState } from 'react';
-import { GetProjectDrops } from './../../../../../../../../../queries/drop.graphql';
-import { ifElse, isNil, always, isEmpty, when } from 'ramda';
-import { Attribute, DropFormState } from '../../../../../../../../../providers/DropFormProvider';
-import { useDropForm } from '../../../../../../../../../hooks/useDropForm';
-import { uploadFile } from '../../../../../../../../../modules/upload';
-import Link from 'next/link';
 import clsx from 'clsx';
-import { shorten } from '../../../../../../../../../modules/wallet';
+import { useDropForm } from '../../../../../../../../../hooks/useDropForm';
+import { StoreApi, useStore } from 'zustand';
+import {
+  DetailSettings,
+  DropFormState,
+  blockchainOptions,
+} from '../../../../../../../../../providers/DropFormProvider';
 
-interface PatchDropData {
-  createProject: PatchDropPayload;
-}
+interface EditDropDetailsPageProps {}
 
-interface PatchDropVars {
-  input: PatchDropInput;
-}
-
-interface GetOrganizationBalanceVars {
-  organization: string;
-}
-
-interface GetOrganizationCreditBalanceData {
-  organization: Organization;
-}
-
-interface GetCreditSheetData {
-  creditSheet: ActionCost[];
-}
-
-export default function EditDropPreviewPage() {
+export default function EditDropDetailsPage({}: EditDropDetailsPageProps) {
   const router = useRouter();
   const { project } = useProject();
-  const [submitting, setSubmitting] = useState(false);
   const store = useDropForm() as StoreApi<DropFormState>;
   const detail = useStore(store, (store) => store.detail);
-  const payment = useStore(store, (store) => store.payment);
-  const timing = useStore(store, (store) => store.timing);
-  const [error, setError] = useState<string>();
+  const setDetail = useStore(store, (store) => store.setDetail);
 
-  const back = () => {
-    router.push(`/projects/${project?.id}/drops/${project?.drop?.id}/edit/schedule`);
+  const { handleSubmit, register, control, setValue, formState, watch, setError, clearErrors } =
+    useForm<DetailSettings>({
+      defaultValues: detail || {},
+    });
+
+  const includeAnimationUrl = watch('includeAnimationUrl');
+
+  const submit = (data: DetailSettings) => {
+    setDetail(data);
+    router.push(`/projects/${project?.id}/drops/${project?.drop?.id}/edit/royalties`);
   };
 
-  const [patchDrop] = useMutation<PatchDropData, PatchDropVars>(PatchDrop, {
-    refetchQueries: [{ query: GetProjectDrops, variables: { project: project?.id } }],
+  const { fields, append, remove } = useFieldArray({
+    control,
+    name: 'attributes',
   });
 
-  if (!detail || !payment || !timing) {
-    toast('Incomplete drops data. Check again.');
-    router.push(`/projects/${project?.id}/drops/${project?.drop?.id}/edit/details`);
-    return;
-  }
-
-  let startDateTime: Date | null = null;
-  if (timing.selectStartDate === 'specifyStartDate' && timing.startTime && timing.startDate) {
-    const [startTimeHrs, startTimeMins] = timing.startTime.split(':');
-    startDateTime = combineDateTime(
-      new Date(timing.startDate),
-      parseInt(startTimeHrs),
-      parseInt(startTimeMins)
-    );
-  }
-
-  let endDateTime: Date | null = null;
-  if (timing.selectEndDate === 'specifyEndDate' && timing.endTime && timing.endDate) {
-    const [endTimeHrs, endTimeMins] = timing.endTime.split(':');
-    endDateTime = combineDateTime(
-      new Date(timing.endDate),
-      parseInt(endTimeHrs),
-      parseInt(endTimeMins)
-    );
-  }
-
-  const onSubmit = async () => {
-    setSubmitting(true);
-    let imageUrl = detail.image;
-    if (detail.image instanceof File) {
-      const { url: image } = await uploadFile(detail.image);
-      imageUrl = image;
-    }
-
-    patchDrop({
-      variables: {
-        input: {
-          id: project?.drop?.id,
-          metadataJson: {
-            name: detail.name,
-            symbol: detail.symbol,
-            description: detail.description,
-            image: imageUrl as string,
-            attributes: detail.attributes.map(({ traitType, value }) => ({ traitType, value })),
-            externalUrl: when(isEmpty, always(null))(detail.externalUrl) as string | null,
-            animationUrl: when(isEmpty, always(null))(detail.animationUrl) as string | null,
-          },
-          creators: payment.creators.map(({ address, share, verified }) => ({
-            address,
-            share,
-            verified,
-          })),
-          price: 0,
-          sellerFeeBasisPoints: ifElse(
-            isNil,
-            always(null),
-            (royalties) => parseFloat(royalties.split('%')[0]) * 100
-          )(payment.royalties),
-          startTime: maybeToUtc(startDateTime),
-          endTime: maybeToUtc(endDateTime),
-        },
-      },
-      onCompleted: () => {
-        setSubmitting(false);
-        router.push(`/projects/${project?.id}/drops`);
-      },
-      onError: (error: ApolloError) => {
-        setSubmitting(false);
-        setError(error.message);
-      },
-    });
-  };
-
   return (
-    <Card className="w-[906px]">
-      <div className="flex gap-8">
-        <div className="basis-1/3 flex flex-col gap-4 w-full">
-          {detail.includeAnimationUrl && detail.animationUrl && (
-            <>
-              <span className="text-sm text-gray-400">Main artwork</span>
-              <video
-                src={detail.animationUrl}
-                controls
-                className="w-full aspect-video object-cover rounded-lg"
+    <>
+      <Card className="w-[492px]">
+        <Typography.Header size={Size.H2}>Drop details</Typography.Header>
+        <Form className="flex flex-col mt-5" onSubmit={handleSubmit(submit)}>
+          <div className="mt-5">
+            <Form.Label
+              name={includeAnimationUrl ? 'Cover image' : 'Main artwork'}
+              className="text-xs text-yellow-300"
+            >
+              <Controller
+                name="image"
+                control={control}
+                rules={{ required: 'Please upload an image.' }}
+                render={({ field: { value, onBlur } }) => (
+                  <Dropzone
+                    noClick
+                    multiple={false}
+                    onDrop={([file], _reject, e) => {
+                      e.preventDefault();
+                      clearErrors('image');
+                      if (file['type'].split('/')[0] !== 'image') {
+                        setError('image', {
+                          message:
+                            'Uploading video files is not currently supported. You can add a link to a hosted video by checking the "Include a video" checkbox below.',
+                        });
+                      } else {
+                        setValue('image', file as unknown as File, { shouldValidate: true });
+                      }
+                    }}
+                  >
+                    {({ getRootProps, getInputProps, isDragActive, open }) => {
+                      return (
+                        <div
+                          {...getRootProps()}
+                          className={clsx(
+                            'flex items-center justify-center border border-dashed border-stone-800 cursor-pointer rounded-md p-6 text-center text-gray-500',
+                            {
+                              'bg-stone-800': isDragActive,
+                            }
+                          )}
+                        >
+                          <input {...getInputProps({ onBlur })} />
+                          {value ? (
+                            <Form.DragDrop.Preview value={value} />
+                          ) : (
+                            <div className="flex flex-col gap-2 text-gray-400">
+                              <p className="text-center">
+                                Drag & drop file or{' '}
+                                <span className="text-yellow-300 cursor-pointer">Browse files</span>{' '}
+                                to upload.
+                                <br />
+                                <br />
+                                JPEG, GIF and PNG supported. Must be under 10 MB.
+                              </p>
+                            </div>
+                          )}
+                        </div>
+                      );
+                    }}
+                  </Dropzone>
+                )}
               />
+            </Form.Label>
+            <Form.Error message={formState.errors.image?.message} />
+          </div>
+
+          <Form.Label name="Include a video" placement={Placement.Right} className="mt-5">
+            <Form.Checkbox {...register('includeAnimationUrl')} />
+          </Form.Label>
+
+          {includeAnimationUrl && (
+            <>
+              <Form.Label name="Video URL" className="text-xs mt-5 basis-3/4">
+                <Form.Input
+                  {...register('animationUrl')}
+                  autoFocus
+                  placeholder="URL to hosted video"
+                />
+              </Form.Label>
             </>
           )}
-          <span className="text-sm text-gray-400">
-            {detail.includeAnimationUrl && detail.animationUrl ? 'Cover image' : 'Main artwork'}
-          </span>
-          <img
-            className="w-full self-center object-contain rounded-lg"
-            src={detail.image instanceof File ? URL.createObjectURL(detail.image) : detail.image}
-          />
-          <div className="grid grid-cols-2 gap-2">
-            {detail.attributes.map((attribute: Attribute) => {
-              return (
+
+          <div className="flex items-center gap-6">
+            <div className="mt-5 basis-3/4 self-baseline">
+              <Form.Label name="Name" className="text-xs">
+                <Form.Input
+                  {...register('name', {
+                    required: 'Please enter a name.',
+                    validate: (value, { blockchain }) => {
+                      if (blockchain.id === Blockchain.Solana && value.length > 32) {
+                        return 'Name length exceeded the limit of 32.';
+                      }
+                    },
+                  })}
+                  autoFocus
+                  placeholder="e.g. Bored Ape Yatch Club"
+                />
+              </Form.Label>
+              <Form.Error message={formState.errors.name?.message} />
+            </div>
+            <div className="mt-5 basis-1/4 self-baseline">
+              <Form.Label name="Symbol" className="text-xs">
+                <Form.Input
+                  {...register('symbol', {
+                    required: 'Symbol required.',
+                    validate: (value, { blockchain }) => {
+                      if (blockchain.id === Blockchain.Solana && value.length > 10) {
+                        return 'Symbol length exceeded the limit of 10.';
+                      }
+                    },
+                  })}
+                  placeholder="e.g. BAYC"
+                />
+              </Form.Label>
+              <Form.Error message={formState.errors.symbol?.message} />
+            </div>
+          </div>
+          <div className="mt-5 flex flex-col gap-1">
+            <span className="form-label-text text-xs">Blockchain</span>
+            <span>{detail?.blockchain.name}</span>
+          </div>
+          <Form.Label name="Description" className="text-xs mt-5">
+            <Form.TextArea
+              {...register('description')}
+              placeholder="Enter a description for the drop."
+            />
+          </Form.Label>
+          <Form.Label name="External URL" className="text-xs mt-5">
+            <Form.Input
+              {...register('externalUrl')}
+              placeholder="Set an external url for the drop."
+            />
+          </Form.Label>
+          <Form.Label name="Attribute" className="text-xs mt-5">
+            {fields.map((field, index) => (
+              <div className="flex gap-6" key={field.id}>
+                <Form.Label name="Trait" className="text-xs basis-1/2">
+                  <Form.Input
+                    {...register(`attributes.${index}.traitType`, { required: true, minLength: 1 })}
+                  />
+                </Form.Label>
+
+                <Form.Label name="Value" className="text-xs basis-1/2">
+                  <Form.Input
+                    {...register(`attributes.${index}.value`, { required: true, minLength: 1 })}
+                  />
+                </Form.Label>
+
                 <div
-                  key={attribute.traitType}
-                  className="flex flex-col gap-2 py-2 px-4 bg-stone-800 rounded-lg col-span-1"
+                  className="rounded-md bg-stone-800 hover:bg-stone-950 p-3 self-end cursor-pointer"
+                  onClick={() => remove(index)}
                 >
-                  <span className="text-gray-400 text-sm">{attribute.traitType}</span>
-                  <span className="text-sm text-white">{attribute.value}</span>
+                  <Icon.Close stroke="stroke-white" />
                 </div>
-              );
-            })}
-          </div>
-        </div>
-        <div className="basis-2/3 flex flex-col w-full">
-          <span className="text-sm text-gray-400">{detail.symbol}</span>
-          <Typography.Header size={Size.H2} className="mt-2">
-            {detail.name}
-          </Typography.Header>
-
-          <div className="flex gap-4 justify-between">
-            <span className="text-sm text-gray-400">{detail.description}</span>
-            <div className="flex flex-col gap-2 py-2 px-4 bg-stone-800 rounded-lg">
-              <span className="text-gray-400 text-sm">Supply</span>
-              <span className="text-sm text-white">
-                {payment.supply ? payment.supply : 'Unlimited'}
-              </span>
-            </div>
-          </div>
-
-          <hr className="w-full bg-stone-800 my-4 h-px border-0" />
-
-          <div className="flex flex-col gap-2 text-white text-sm w-full">
-            <div className="flex items-center justify-between gap-2">
-              <span>Royalties</span>
-              <span>{payment.royalties}</span>
-            </div>
-            <div className="flex items-center justify-between gap-2">
-              <span>Royalties recipients</span>
-              <div className="flex flex-col gap-2 justify-end">
-                {payment.creators.map((creator: CreatorInput) => {
-                  return (
-                    <div key={creator.address}>{`${shorten(creator.address)} - ${
-                      creator.share
-                    }%`}</div>
-                  );
-                })}
               </div>
-            </div>
-            <div className="flex items-center justify-between gap-2">
-              <span>Starts</span>
-              <span>
-                {startDateTime
-                  ? `${format(startDateTime, DateFormat.DATE_1)}, ${format(
-                      startDateTime,
-                      DateFormat.TIME_1
-                    )}`
-                  : 'Immediately'}
-              </span>
-            </div>
-            <div className="flex items-center justify-between gap-2">
-              <span>Ends</span>
-              <span>
-                {endDateTime
-                  ? `${format(endDateTime, DateFormat.DATE_1)}, ${format(
-                      endDateTime,
-                      DateFormat.TIME_1
-                    )}`
-                  : 'Never'}
-              </span>
-            </div>
-            {detail.externalUrl && (
-              <div className="flex items-center justify-between gap-2">
-                <span>External URL</span>
-                <span>{detail.externalUrl}</span>
-              </div>
-            )}
-            <div className="flex items-center justify-between gap-2">
-              <span>Blockchain</span>
-              <span>{detail.blockchain.name}</span>
-            </div>
-          </div>
+            ))}
+          </Form.Label>
 
-          <hr className="w-full bg-stone-800 my-4 h-px border-0" />
-
-          {error && (
-            <div className="text-sm font-medium text-red-500 bg-red-500/25 p-4 my-5 rounded-lg">
-              {error}
-            </div>
-          )}
-
-          <div className="flex items-center justify-end gap-6 mt-4">
-            <Button variant="secondary" disabled={submitting} onClick={back}>
-              Back
-            </Button>
-            <Button htmlType="submit" loading={submitting} disabled={submitting} onClick={onSubmit}>
-              {startDateTime ? 'Schedule drop' : 'Update drop'}
-            </Button>
-          </div>
-        </div>
-      </div>
-    </Card>
+          <Button
+            className="mt-4 self-start"
+            variant="secondary"
+            onClick={() => append({ traitType: '', value: '' })}
+          >
+            Add attribute
+          </Button>
+          <hr className="w-full bg-stone-800 border-0 h-px my-5" />
+          <Button
+            htmlType="submit"
+            className="self-end"
+            loading={formState.isSubmitting}
+            disabled={formState.isSubmitting}
+          >
+            Next
+          </Button>
+        </Form>
+      </Card>
+    </>
   );
 }
