1da5aa157312fcf0c370ab0780ec451d36d175cf
mpw <x@mpw.sh>
2023-05-26 16:05:14 -0300

    fix recovery flow (#201)
    * fix recovery flow
    
    * update kratos settings
    
    * update skaffold yaml
    
    * null flow issue resolved
    
    * flow and redirect fixes
    
    * add confirm password
    
    * fix: loading state for login and autofocus
    
    ---------
    
    Co-authored-by: Anshul Goel <anshul.go87@gmail.com>
    Co-authored-by: Kyle Espinola <kyle.s.espinola@gmail.com>

--- src/hooks/useRecoveryCode.ts
+++ src/hooks/useRecoveryCode.ts
@@ -3,6 +3,7 @@ import { useRouter } from 'next/navigation';
 import { extractFlowNode } from '../modules/ory';
 import { useOry } from './useOry';
 import { FormState, useForm, UseFormHandleSubmit, UseFormRegister } from 'react-hook-form';
+import { toast } from 'react-toastify';
 
 interface RecoveryForm {
   code: string;
@@ -17,13 +18,11 @@ interface RecoveryCodeContext {
 
 interface RecoveryCodeProps {
   flow: RecoveryFlow | undefined;
-  email: string;
 }
 
-export function useRecoveryCode({ flow, email }: RecoveryCodeProps): RecoveryCodeContext {
+export function useRecoveryCode({ flow }: RecoveryCodeProps): RecoveryCodeContext {
   const router = useRouter();
   const { ory } = useOry();
-
   const { register, handleSubmit, formState, setError } = useForm<RecoveryForm>();
 
   const onSubmit = async (values: RecoveryForm): Promise<void> => {
@@ -35,24 +34,28 @@ export function useRecoveryCode({ flow, email }: RecoveryCodeProps): RecoveryCod
         extractFlowNode('csrf_token')(flow.ui.nodes).attributes as UiNodeInputAttributes
       ).value;
 
-      const { data } = await ory.updateRecoveryFlow({
+      const result = await ory.updateRecoveryFlow({
         flow: flow.id,
-        updateRecoveryFlowBody: { ...values, email, csrf_token: csrfToken, method: 'code' },
+        updateRecoveryFlowBody: { code: values.code.trim(), csrf_token: csrfToken, method: 'code' },
       });
 
-      router.push('/recovery/reset?flow=' + data.id + '&email=' + email);
+      if (result.data.ui != undefined) {
+        const codeErr = result.data?.ui?.messages?.[0]?.text;
+        if (codeErr) {
+          setError('code', { message: codeErr });
+          toast.error(codeErr);
+        }
+      }
     } catch (err: any) {
-      const {
-        response: {
-          data: {
-            ui: { nodes },
-          },
-        },
-      } = err;
-      const codeErr = extractFlowNode('code')(nodes).messages[0]?.text;
-
-      if (codeErr) {
-        setError('code', { message: codeErr });
+      if (err?.response?.status === 422) {
+        const redirectUrl = err?.response?.data?.redirect_browser_to;
+        if (redirectUrl) {
+          router.push(`/recovery/reset?flow=${flow.id}`);
+        } else {
+          toast.error('No redirect URL found in error response');
+        }
+      } else {
+        toast.error(err?.response?.data?.error.message);
       }
     }
   };
