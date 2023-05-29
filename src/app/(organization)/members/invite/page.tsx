5f236313841e69bb2d4129e06636ef5e8333f44a
Anshul Goel <anshul.go87@gmail.com>
2023-05-29 19:57:26 +0530

    cleanup

--- src/app/(organization)/members/invite/page.tsx
+++ src/app/(organization)/members/invite/page.tsx
@@ -2,10 +2,6 @@
 
 import InviteMember from '../InviteMember';
 
-interface InviteMemberForm {
-  email: string;
-}
-
 export default function MemberInvitePage() {
   return (
     <div className="h-screen flex items-center justify-center">
