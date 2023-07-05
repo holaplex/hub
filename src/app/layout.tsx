fc71c86c8bf0ee7e2e135b70c4f5ed341e247485
Anshul Goel <anshul.go87@gmail.com>
2023-07-05 17:42:42 +0530

    Add termly script (#221)
    * add termly script
    
    * fetch id from env varialbles
    
    * feat: add termly enviroment to the docker image when building with github actions
    
    ---------
    
    Co-authored-by: Kyle Espinola <kyle.s.espinola@gmail.com>

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import App from './App';
 import { cookies } from 'next/headers';
 import { ory, serverConfig } from '../modules/ory';
 import { Session } from '@ory/client';
+import Script from 'next/script';
 
 export const metadata = {
   title: 'Holaplex Hub',
@@ -15,6 +16,8 @@ const client = ory(serverConfig);
 export default async function RootLayout({ children }: { children: React.ReactNode }) {
   const cookStore = cookies();
   let session: Session | undefined = undefined;
+  const showCookieConsentBanner = process.env.NEXT_PUBLIC_COOKIE_CONSENT_BANNER;
+  const termlyId = process.env.NEXT_PUBLIC_TERMLY_ID;
 
   try {
     const { data } = await client.toSession({
@@ -28,6 +31,14 @@ export default async function RootLayout({ children }: { children: React.ReactNo
     <html lang="en">
       <body>
         <App session={session}>{children}</App>
+        {showCookieConsentBanner && (
+          <Script
+            src="https://app.termly.io/embed.min.js"
+            type="text/javascript"
+            data-auto-block="on"
+            data-website-uuid={termlyId}
+          />
+        )}
       </body>
     </html>
   );
