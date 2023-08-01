3762e3414b7b4fee225d562f5097edb8385a49e0
mpw <x@mpw.sh>
2023-08-01 17:27:09 -0300

    add missing -mint- action, fix typo on retry collection (#264)

--- src/app/(organization)/credits/constant.ts
+++ src/app/(organization)/credits/constant.ts
@@ -3,11 +3,12 @@ import { Action } from './../../../graphql.types';
 export const ACTION_LABEL = {
   [Action.CreateDrop]: 'Create Drop',
   [Action.MintEdition]: 'Mint Edition',
+  [Action.Mint]: 'Mint',
   [Action.RetryMint]: 'Retry Mint',
   [Action.TransferAsset]: 'Transfer Asset',
   [Action.CreateWallet]: 'Create Wallet',
   [Action.RetryDrop]: 'Retry Drop',
   [Action.CreateCollection]: 'Create Collection',
   [Action.MintCompressed]: 'Mint Compressed',
-  [Action.RetryCollection]: 'Retyry Collection',
+  [Action.RetryCollection]: 'Retry Collection',
 };
